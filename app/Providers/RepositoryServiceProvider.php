d9203be6a340ecebd3824f4bb7d822c95c024d4e
ravalison99 <ravalisondaddyluciano@gmail.com>
2026-07-29 11:11:15 +0400

    S2 : Repository Pattern pour User

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -11,7 +11,10 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+        \App\Repositories\Contracts\UserRepositoryInterface::class,
+        \App\Repositories\Eloquent\EloquentUserRepository::class
+        );
     }
 
     /**
