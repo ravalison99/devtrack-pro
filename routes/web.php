ba6dcb7951cdd11c061ecef60bb1510922a046fa
ravalison99 <ravalisondaddyluciano@gmail.com>
2026-07-30 16:01:44 +0400

    S2: AuthService, UserService, AuthController, routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', fn () => 'Tableau de bord (à construire)')->name('dashboard');
 });
